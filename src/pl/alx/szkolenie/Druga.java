c8d151e5e69c643a42068354912ec5dd8d2545cd
kurs <kurs@kurs-Komputer>
2017-11-22 15:36:17 +0100

    Dodana druga Klasa o nazwie Druga
    W drugiej klasie tez dajemy czadu.
    
    Signed-off-by: kurs <kurs@kurs-Komputer>

---
+++ src/pl/alx/szkolenie/Druga.java
@@ -0,0 +1,10 @@
+package pl.alx.szkolenie;
+
+public class Druga {
+
+	public static void main(String[] args) {
+		System.out.println("W klasie drugiej tez dajemy czadu !!!!!");
+
+	}
+
+}
--- src/pl/alx/szkolenie/Pierwsza.java
+++ src/pl/alx/szkolenie/Pierwsza.java
@@ -4,6 +4,7 @@ public class Pierwsza {
 
 	public static void main(String[] args) {
 	
+		System.out.println("Idziemy po bandzie");
 
 	}
 
